2487d831fea66be7df718948c03c21cc4527a180
PPasson <passonpp@gmail.com>
2023-02-17 12:13:07 +0700

    add load more button and function

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <NavBar />
     <div class="container">
-      <div class="input-group mb-3 pt-2">
+      <div class="input-group mb-3 pt-4" style="max-width: 30rem">
         <input
           type="text"
           class="form-control"
@@ -20,13 +20,15 @@
         </div>
       </div>
 
-      <div v-if="loading">Loading...</div>
+      <div v-if="loading"><img src="~/assets/foodLoader.gif" /></div>
 
       <div v-else>
         <div v-for="(result, index) in results" :key="index">
           <b-card class="m-3">
-            <h3>
-              {{ result.name }}
+            <div class="fluid">
+              <h3>
+                {{ result.name }}
+              </h3>
               <b-form-rating
                 id="rating-inline"
                 inline
@@ -35,11 +37,26 @@
                 v-model="result.rating"
                 variant="warning"
                 readonly
+                class="ml-n3"
               ></b-form-rating>
-            </h3>
+              <span
+                v-if="result.opening_hours && result.opening_hours.open_now"
+                class="openNow"
+                >open now</span
+              >
+            </div>
             <p>{{ result.formatted_address }}</p>
           </b-card>
         </div>
+      </div>
+      <div v-if="nextPageToken" class="p-4">
+        <button
+          class="btn btn-primary"
+          type="button"
+          @click="loadMoreResults()"
+        >
+          Load More
+        </button>
       </div>
     </div>
   </div>
@@ -53,6 +70,7 @@ export default {
       keyword: "",
       results: [],
       loading: false,
+      nextPageToken: null,
     };
   },
   methods: {
@@ -69,14 +87,42 @@ export default {
       });
       this.loading = false;
       this.results = response.results;
+      this.nextPageToken = response.next_page_token;
       console.log("result = ", this.results);
+    },
+    async loadMoreResults() {
+      this.loading = true;
+      const apiKey = process.env.GOOGLE_MAP_API_KEY;
+      const response = await this.$axios.$get("/maps/place/textsearch/json", {
+        params: {
+          pagetoken: this.nextPageToken,
+          key: apiKey,
+        },
+      });
+      this.loading = false;
+      this.results = this.results.concat(response.results);
+      this.nextPageToken = response.next_page_token;
     },
   },
 };
 </script>
 
 <style scoped>
-  .container{
-    max-width: 1080px;
+.container {
+  max-width: 1080px;
+  display: grid;
+  justify-items: center;
+}
+.openNow {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px green, 0 0 5px darkgreen;
+  animation: blinker 0.7s linear infinite;
+  margin-right: 1rem;
+}
+@keyframes blinker {
+  50% {
+    opacity: 50%;
+    text-shadow: 1px 1px 2px black, 0 0 25px yellowgreen, 0 0 5px darkgoldenrod;
   }
+}
 </style>
