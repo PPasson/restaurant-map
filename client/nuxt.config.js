6ff3046686e7f2e5c2ffad4b6d1d8a047b3ac901
PPasson <passonpp@gmail.com>
2023-02-17 17:22:26 +0700

    add axios proxy true to fix CORS problem

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -42,6 +42,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+  axios: {
+    proxy: true
+  },
   proxy: {
     '/maps': {
       target: 'https://maps.googleapis.com/maps/api',
