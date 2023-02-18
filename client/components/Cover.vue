86876818cf2d06b26ffcdbc528300a2a82036824
PPasson <passonpp@gmail.com>
2023-02-18 15:24:36 +0700

    change cover picture

--- client/components/Cover.vue
+++ client/components/Cover.vue
@@ -1,15 +1,25 @@
 <template>
-  <dir>
-    
-  </dir>
+  <div align="center">
+      <b-img src="~/assets/food-cover.jpeg" fluid-grow alt="Responsive image" class="cover"></b-img>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+.nav {
+  background-image: linear-gradient(rgb(56, 189, 248), rgb(186, 230, 253));
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+}
 
+h2 {
+  color: white;
+  text-shadow: 0px 1px 1px #000000;
+}
+.cover{
+  max-height: 15rem;
+  max-width: 2000px;
+}
 </style>
